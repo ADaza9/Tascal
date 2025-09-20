6e1168670a1cc534e1aee105533f898c4a8563dc
Armando Daza <armando.daza.92@gmail.com>
2025-09-20 18:36:02 -0500

    Configuracion del archivo next

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
  env: {
-   DATABASE_URL: process.env.DATABASE_URL,
+   DATABASE_URL: process.env.DATABASE_URL!,
  },
 };
 
